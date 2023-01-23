9ca30f2d5e4b33402826aebb0037cd864895145f
Fayven <fayven.amelga@gmail.com>
2023-01-23 16:02:01 -0500

    Add simple syntax to README

--- index.js
+++ index.js
@@ -1,3 +1,69 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
+const readMe = `# Your Project Title
+
+## Description 
+
+${description}
+
+
+## Table of Contents (Optional)
+
+${Contents}
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+
+
+## Installation
+
+${installation}
+
+## Usage 
+
+${usage}
+
+To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
+
+```md
+![alt text](assets/images/screenshot.png)
+```
+
+
+## Credits
+
+${credits}
+
+
+## License
+
+${license} [https://choosealicense.com/](https://choosealicense.com/)
+
+
+---
+
+🏆 The sections listed above are the minimum for a good README, but your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
+
+## Badges
+
+![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
+
+Badges aren't _necessary_, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+
+## Features
+
+If your project has a lot of features, consider adding a heading called "Features" and listing them there.
+
+## Contributing
+
+If you created an application or package and would like other developers to contribute it, you will want to add guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own.
+
+## Tests
+
+Go the extra mile and write tests for your application. Then provide examples on how to run them.
+
+---
+
+© 2022 Trilogy Education Services, LLC, a 2U, Inc. brand. Confidential and Proprietary. All Rights Reserved.`
