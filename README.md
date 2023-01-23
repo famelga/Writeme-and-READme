e5cab3683ba7100a8c18d3c6f468e1f3316af3c3
Fayven <fayven.amelga@gmail.com>
2023-01-23 18:57:35 -0500

    add fs writefile

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# Writeme-and-READme
+
+        
+
+        a
+        ## Description 
+        
+        a
+        
+        
+        ## Table of Contents (Optional)
+        
+        a
+        * [Installation](answer.installation)
+        * [Usage](answer.usage)
+        * [Credits](answer.credits)
+        * [License](answer.license)
+        
+        
+        ## Installation
+        
+        a
+        
+        ## Usage 
+        
+        a
--- index.js
+++ index.js
@@ -1,76 +1,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-// const { generate } = require("rxjs");
-// const { title } = require("process");
-
-const readMe = `
-# Your Project Title
-
-## Description 
-
-${description}
-
-
-## Table of Contents (Optional)
-
-${Contents}
-* [Installation](#installation)
-* [Usage](#usage)
-* [Credits](#credits)
-* [License](#license)
-
-
-## Installation
-
-${installation}
-
-## Usage 
-
-${usage}
-
-To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:`
-
-// ```md
-// ![alt text](assets/images/screenshot.png)
-// ```
-
-
-// ## Credits
-
-// ${credits}
-
-
-// ## License
-
-// ${license} [https://choosealicense.com/](https://choosealicense.com/)
-
-
-// ---
-
-// 🏆 The sections listed above are the minimum for a good README, but your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
-
-// ## Badges
-
-// ![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
-
-// Badges aren't _necessary_, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
-
-// ## Features
-
-// If your project has a lot of features, consider adding a heading called "Features" and listing them there.
-
-// ## Contributing
-
-// If you created an application or package and would like other developers to contribute it, you will want to add guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own.
-
-// ## Tests
-
-// Go the extra mile and write tests for your application. Then provide examples on how to run them.
-
-// ---
-
-// © 2022 Trilogy Education Services, LLC, a 2U, Inc. brand. Confidential and Proprietary. All Rights Reserved.`;
-
+// const { title } = require('process');
 
 function writeReadMe() {
     inquirer
@@ -102,16 +32,77 @@ function writeReadMe() {
        },
     ])
     .then ((answer) => {
+        
         console.log(answer.title);
         console.log(answer.description);
-        console.log(answer.contents);
+        console.table(answer.contents);
         console.log(answer.installation);
         console.log(answer.usage);
         console.log(answer.credits);
+
+        var readMe = (`
+        
+
+        ${answer.title}
+        ## Description 
+        
+        ${answer.description}
+        
+        
+        ## Table of Contents (Optional)
+        
+        ${answer.contents}
+        * [Installation](answer.installation)
+        * [Usage](answer.usage)
+        * [Credits](answer.credits)
+        * [License](answer.license)
+        
+        
+        ## Installation
+        
+        ${answer.installation}
+        
+        ## Usage 
+        
+        ${answer.usage}`
+   )
+   fs.writeFile('README.md', readMe, (err) => {
+    if (err) throw err;
+    console.log('README.md created');
+   })
     })
 
 }
 
+// ${} answers to readme template
+
+// fs.writeFile(answers.title) {
+//     ${#title}
+
+
+    // ## Description 
+    
+    // ${description}
+    
+    
+    // ## Table of Contents (Optional)
+    
+    // ${Contents}
+    // * [Installation](#installation)
+    // * [Usage](#usage)
+    // * [Credits](#credits)
+    // * [License](#license)
+    
+    
+    // ## Installation
+    
+    // ${installation}
+    
+    // ## Usage 
+    
+    // ${usage}'
+
+// }
 // function renderLicenseBadge(license) {
 
 // }
